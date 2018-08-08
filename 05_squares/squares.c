20ee1c9752966b1de3d3ced6670c449128e3bdaf
Coursera Learner <nobody@nowhere.nul>
2018-08-08 03:15:23 +0000

    Squares

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#define MAX(x,y) ((x>y)?x:y)
+#define GREATER(x,y) ((x>y)?1:0))
+#define LESS(x,y) ((x<y)?1:0)
+#define GREATERE(x,y) ((x=>y)?1:0)
+#define LESSE(x,y) ((x<=y)? 1:0)
 void squares(int size1, int x_offset, int y_offset, int size2) {
-  //compute the max of size1 and (x_offset + size2).  Call this w
-
+ //compute the max of size1 and (x_offset + size2).  Call this w
+  int w,h,aux1,aux2,aux3,aux4;
+  w = MAX(size1, (x_offset + size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  h = MAX(size1, (y_offset + size2));
+  
   //count from 0 to h. Call the number you count with y
-
+  for(int y = 0; y < h; y++){
     //count from 0 to w. Call the number you count with x
+    for(int x = 0; x<w; x++){
+      aux1 = (GREATERE(x,x_offset)&&(LESSE(x,x_offset+size2)));
+      aux2 = ((y==y_offset)|(y == (y_offset+size2-1)));
+      aux3 = (GREATERE(y,y_offset)&&(LESSE(y,y_offset+size2)));
+      aux4 = ((x==x_offset)|(x == (x_offset+size2-1)));
+      if((aux1 && aux2)|(aux3&&aux4)) printf("*");
+      else if (((x<size1)&&((y==0)|(y==(size1-1)))) | ((y < size1) && ((x == 0) | (x == (size1-1))))) printf("#");
+      else printf(" ");
+    }
+    printf("\n");
+  }
+   
 
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
