5abbdf7106f80bb9e1faf0f47d233b18b08f0942
Coursera Learner <nobody@nowhere.nul>
2018-08-17 12:02:10 +0000

    agoravia

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -10,16 +10,15 @@ void assert_card_valid(card_t c) {
 
 const char * ranking_to_string(hand_ranking_t r) {
   switch(r){
-  case STRAIGHT_FLUSH: printf("STRAIGHT_FLUSH\n"); break;
-  case FOUR_OF_A_KIND: printf("FOUR OF A KIND"); break;
+  case STRAIGHT_FLUSH: printf("STRAIGHT_FLUSH"); break;
+  case FOUR_OF_A_KIND: printf("FOUR_OF_A_KIND\n"); break;
   case FULL_HOUSE: printf("FULL HOUSE"); break;
   case FLUSH: printf("FLUSH"); break;
   case STRAIGHT: printf("STRAIGHT"); break;
   case THREE_OF_A_KIND: printf("THREE OF A KIND"); break;
   case TWO_PAIR: printf("TWO PAIR"); break;
-  case PAIR: printf("PAIR"); break;
-  case NOTHING: printf("NOTHING"); break;
-  default: printf("Invalid Input"); break;
+  case PAIR: printf("PAIR\n"); break;
+  case NOTHING: printf("NOTHING\n"); break;
   }
   return "";
 }
