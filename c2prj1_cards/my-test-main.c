b1c612f98b03b382fc6aab11a01c9c9b5d3b57f5
Coursera Learner <nobody@nowhere.nul>
2018-08-17 06:03:12 +0000

    maybe the end

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,21 @@
 #include "cards.h"
-
+#include <stdio.h>
 int main(void) {
-
+  card_t c,h;
+  c.value = 10;
+  c.suit = DIAMONDS;
+  char d;
+  hand_ranking_t r = FLUSH;
+  assert_card_valid(c);
+  printf("Is valid\n");
+  ranking_to_string(r);
+  d = value_letter(c);
+  printf("%c\n",d);
+  d = suit_letter(c);
+  printf("%c\n",d);
+  print_card(c);
+  h = card_from_letters('0', 'd');
+  printf("\n%d%d\n",h.value,h.suit);
+  h = card_from_num(0);
+  printf("%d%d\n",h.value,h.suit);
 }
